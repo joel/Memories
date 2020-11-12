c2631f9b62c30def783ab88c0f26ca0258b18232
Joel Azemar <joel.azemar@gmail.com>
2020-11-12 13:31:44 +0100

    10. Using Native Device Features > native-06-preparing-the-new-memory-page

--- src/pages/NewMemory.tsx
+++ src/pages/NewMemory.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
+
+import { camera } from 'ionicons/icons';
+
+import '../theme/NewMemory.css';
 
 const NewMemory: React.FC = () => {
   return (
@@ -22,7 +34,32 @@ const NewMemory: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2>New Memory</h2>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position='floating'>New Memory</IonLabel>
+                <IonInput type='text'></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow className='ion-text-center'>
+            <IonCol>
+              <div className='image-preview'>
+                <h3>No Photo Chosen.</h3>
+              </div>
+              <IonButton fill='clear'>
+                <IonIcon icon={camera} slot='start' />
+                <IonLabel>Take Photo</IonLabel>
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow className='ion-margin-top'>
+            <IonCol className='ion-text-center'>
+              <IonButton>Add Memory</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
