21e013b8fbdd2cfb25209f0afa6992efd44796fd
Joel Azemar <joel.azemar@gmail.com>
2020-11-18 16:07:20 +0100

    Update tabs.spec.js

--- cypress/integration/app/tabs.spec.js
+++ cypress/integration/app/tabs.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const { debug } = require("console");
+
 context('Tabs', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8100');
@@ -7,31 +9,44 @@ context('Tabs', () => {
 
   it('Default Page', () => {
     cy.contains("Good Memories");
-    cy.get('ion-title').should('contain', 'Good Memories');
+    cy.get('ion-title')
+      .should('be.visible')
+      .should('contain', 'Good Memories');
     cy.url().should('include', '/good-memories');
   })
 
   it('Bad Memories', () => {
-    cy.get('#tab-button-bad').click();
+    cy.get('#tab-button-bad')
+      .should('be.visible').click();
     cy.contains("Bad Memories");
-    cy.get('ion-title').should('contain', 'Bad Memories');
+    cy.get('ion-title')
+      .should('be.visible')
+      .should('contain', 'Bad Memories');
     cy.url().should('include', '/bad-memories');
   })
 
   it('New Memory', () => {
-    cy.get('#new-memory-button').click({force: true});
+    cy.get('#new-memory-button')
+      .should('be.visible')
+      .click({force: true});
     cy.addTitle('That day');
   })
 
   it('Take A Picture', () => {
-    cy.get('#new-memory-button').click({force: true});
+    cy.get('#new-memory-button')
+      .should('be.visible')
+      .click({force: true});
     cy.addTitle('That day');
 
-    cy.get('ion-select.md').click();
+    // Select Bad Memory In The Drop Down Menu
+    cy.get('ion-select.md').click(); // Open Modal
+    // Select Bad Memory
     cy.get('#alert-input-1-1 > .alert-button-inner > .alert-radio-icon').click();
+    // Validate
+    // cy.get('.alert-button-group > :nth-child(2)').click();
+    cy.contains('OK').should('be.visible').click();
 
-    cy.get('.alert-button-group > :nth-child(2)').click();
-
-    cy.get('#add-memory-btn').click();
+    // cy.get('#add-memory-btn').debug().click();
+    cy.contains('Add Memory').should('be.visible').click();
   })
 })
